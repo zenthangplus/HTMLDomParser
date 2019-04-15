56d39d70c6874674481557dca0068d8afa88335d
zenthangplus <zenthangplus@gmail.com>
2019-04-15 20:14:24 +0700

    Finish DomTest

--- tests/Helpers/ReflectionHelper.php
+++ tests/Helpers/ReflectionHelper.php
@@ -12,16 +12,32 @@ trait ReflectionHelper
      * Call protected/private method of a class.
      *
      * @param $object
-     * @param $methodName
+     * @param string $methodName
      * @param array $parameters
      * @return mixed
      * @throws \ReflectionException
      */
-    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    public function invokeMethod(&$object, $methodName, $parameters = array())
     {
         $reflection = new \ReflectionClass(get_class($object));
         $method = $reflection->getMethod($methodName);
         $method->setAccessible(true);
         return $method->invokeArgs($object, $parameters);
+    }
+
+    /**
+     * Get invisible property value of a class
+     *
+     * @param $object
+     * @param string $propertyName
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    public function getInvisibleProperty(&$object, $propertyName)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+        return $property->getValue($object);
     }
 }
--- tests/Units/DomTest.php
+++ tests/Units/DomTest.php
@@ -2,23 +2,23 @@
 
 namespace HTMLDomParserTests\Units;
 
+use PHPUnit\Framework\TestCase;
 use HTMLDomParser\Dom;
+use HTMLDomParser\Node;
 use HTMLDomParser\Sources\simple_html_dom;
 use HTMLDomParser\Sources\simple_html_dom_node;
 use HTMLDomParserTests\Helpers\ReflectionHelper;
-use PHPUnit\Framework\TestCase;
 
 /**
  * Class DomTest
  * @package HTMLDomParserTests
  * @covers \HTMLDomParser\Dom
- * @depends NodeTest
  */
 class DomTest extends TestCase
 {
     use ReflectionHelper;
 
-    public $filepath = '/fixtures/document.html';
+    public $filepath = 'fixtures/document.html';
 
     public $html = '<div id="test"><a href="#">Test link</a></div>';
 
@@ -27,12 +27,18 @@ class DomTest extends TestCase
      */
     public $dom;
 
+    /**
+     * Init resources for this tests
+     */
     public function setUp()
     {
         $this->dom = new Dom();
-        $this->dom->loadFile(dirname(__FILE__) . $this->filepath);
+        $this->dom->loadFile(dirname(__FILE__) . '/' . $this->filepath);
     }
 
+    /**
+     * Destroy resources after test finished
+     */
     public function tearDown()
     {
         unset($this->dom);
@@ -40,6 +46,9 @@ class DomTest extends TestCase
 
     /**
      * Test load DOM form file
+     *
+     * @covers \HTMLDomParser\Dom::__construct
+     * @covers \HTMLDomParser\Dom::loadFile
      */
     public function testLoadDomFromFile()
     {
@@ -48,6 +57,9 @@ class DomTest extends TestCase
 
     /**
      * Test load DOM by html string
+     *
+     * @covers \HTMLDomParser\Dom::__construct
+     * @covers \HTMLDomParser\Dom::load
      */
     public function testLoadDomByString()
     {
@@ -57,11 +69,63 @@ class DomTest extends TestCase
 
     /**
      * Test load DOM from simple_html_dom object
+     *
+     * @covers \HTMLDomParser\Dom::__construct
+     * @covers \HTMLDomParser\Dom::loadObject
      */
     public function testLoadDomFromObject()
     {
         $object = new simple_html_dom($this->html);
         $dom = new Dom($object);
         $this->assertInstanceOf(simple_html_dom_node::class, $dom->getSimpleNode());
+    }
+
+    /**
+     * Test set callback function
+     *
+     * @covers \HTMLDomParser\Dom::setCallback
+     * @throws \ReflectionException
+     */
+    public function testSetCallback()
+    {
+        $this->dom->setCallback(function ($node) {
+        });
+        $simpleDom = $this->getInvisibleProperty($this->dom, "dom");
+        $callback = $this->getInvisibleProperty($simpleDom, "callback");
+        $this->assertInstanceOf(\Closure::class, $callback);
+    }
+
+    /**
+     * Test remove callback
+     *
+     * @depends testSetCallback
+     * @covers  \HTMLDomParser\Dom::removeCallback
+     * @throws \ReflectionException
+     */
+    public function testRemoveCallback()
+    {
+        $this->dom->setCallback(function ($node) {
+        });
+        $this->dom->removeCallback();
+        $simpleDom = $this->getInvisibleProperty($this->dom, "dom");
+        $callback = $this->getInvisibleProperty($simpleDom, "callback");
+        $this->assertNull($callback);
+    }
+
+    /**
+     * Test dom's callback called with correct argument
+     *
+     * @depends testSetCallback
+     * @covers \HTMLDomParser\Dom::setCallback
+     */
+    public function testCallbackCalledWithCorrectArgument()
+    {
+        $nodeCalled = null;
+        $dom = new Dom('<strong>test</strong>');
+        $dom->setCallback(function ($node) use (&$nodeCalled) {
+            $nodeCalled = $node;
+        });
+        $_ = (string)$dom;
+        $this->assertInstanceOf(Node::class, $nodeCalled);
     }
 }
