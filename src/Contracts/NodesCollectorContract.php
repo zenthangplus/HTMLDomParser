5d45be9ab4b912f6912090f7c01f76f9e52d28f1
thangbx <thangbx@coccoc.com>
2019-04-26 10:49:03 +0700

    Update interface docs for NodesContract

--- src/Contracts/NodesCollectorContract.php
+++ src/Contracts/NodesCollectorContract.php
@@ -12,8 +12,17 @@ interface NodesCollectorContract extends Iterator, \Countable
 {
     /**
      * Get the number of nodes
+     * @link https://php.net/manual/en/countable.count.php
      *
      * @return int
      */
     public function count();
+
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     *
+     * @return NodeContract
+     */
+    public function current();
 }
